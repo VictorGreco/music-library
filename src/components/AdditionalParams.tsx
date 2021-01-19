123fb317567db6e24e33e217d4a135cc545f5bcd
yogi <raoyogesh78@yahoo.in>
2021-01-19 13:13:44 +0100

    added additionalParams.css

--- src/components/AdditionalParams.tsx
+++ src/components/AdditionalParams.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import Dropdown from './Dropdown';
 import FilterCard from './FilterCard';
+import './AdditionalParams.css';
 
 class AdditionalParams extends React.Component {
     render(): JSX.Element {
         return (
             <div className="additionalParams">
                 <div className="additionalParams_header">
-                    <span className="additionalParams_header_line"></span>
-                    <span className="additionalParams_header_title">ADDITIONAL PARAMETERS</span>
-                    <span className="additionalParams_header_line"></span>
+                    <div className="additionalParams_header_line"></div>
+                    <p className="additionalParams_header_title">ADDITIONAL PARAMETERS</p>
+                    <div className="additionalParams_header_line"></div>
                 </div>
                 <div className="additionalParams_filters">
                     <FilterCard title="Country">
