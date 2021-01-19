b33aff0fb8292227a3883ffc7e94b0e63e4768cd
yogi <raoyogesh78@yahoo.in>
2021-01-19 11:51:03 +0100

    added search component

--- src/App.tsx
+++ src/App.tsx
@@ -3,26 +3,15 @@ import ReactDom from 'react-dom';
 import logo from './logo.svg';
 import './App.css';
 import env from 'react-dotenv';
+import Search from './components/Search';
 
 const jsonp = require('jsonp');
 
-function App() {
+function App(): JSX.Element {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        </a>
-      </header>
+      <Search />
     </div>
   );
 }
---
+++ src/components/Search.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Search extends React.Component {
+    render(): JSX.Element {
+        return (
+            <div className="search_container">
+                <h1>Music Library</h1>
+                
+            </div>
+        );
+    }
+}
+
+export default Search;
