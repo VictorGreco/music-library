67e8cea9a135455c9714de5374553ab9127617a5
yogi <raoyogesh78@yahoo.in>
2021-01-19 13:33:27 +0100

    added filterCard.css

--- src/components/FilterCard.tsx
+++ src/components/FilterCard.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import './FilterCard.css';
+import Input from './Input';
 
 function FilterCard({ title, children}: { title: string; children: JSX.Element}): JSX.Element {
     return (
         <div className="filterCard">
+            <div className="filterCard_checkbox">
+                <Input type="checkbox" placeholder=""/>
+            </div>
             <p className="filterCard_title">{title}</p>
             {children}
         </div>
