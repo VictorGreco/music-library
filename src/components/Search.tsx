7a5c8fc707d8739c1d660520f00278f48299b5b1
yogi <raoyogesh78@yahoo.in>
2021-01-19 12:28:39 +0100

    added search.css

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import Dropdown from './Dropdown';
 import Input from './Input';
+import './Search.css';
 
 class Search extends React.Component {
     render(): JSX.Element {
         return (
             <div className="search_container">
-                <h1>Music Library</h1>
-                <Dropdown options={['Song', 'Artist', 'Album', 'Music Video']}/>
-                <Input type="text" placeholder="name..." />
+                <div className="search_container_title">
+                    <h1>Music Library</h1>
+                </div>
+                <div className="search_container_fields">
+                    <Dropdown options={['Song', 'Artist', 'Album', 'Music Video']}/>
+                    <Input type="text" placeholder="name..." />
+                </div>
             </div>
         );
     }
