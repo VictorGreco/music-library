5cc4a736602c086ec175661a667deaf633bba7dc
yogi <raoyogesh78@yahoo.in>
2021-01-19 12:40:49 +0100

    added key to dropdown options

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import './Dropdown.css';
 
 function Dropdown({ options }: { options: string[] }): JSX.Element {
-    const optionsList = options.map((option: string): JSX.Element => {
-        return <option value={option.toLowerCase()}>{option}</option>
+    const optionsList = options.map((option: string, index: number): JSX.Element => {
+        return <option key={index} value={option.toLowerCase()}>{option}</option>
     })
 
     return (
