565ca1fa54a265ebde64f07edd1610bf259e8493
yogi <raoyogesh78@yahoo.in>
2021-01-20 12:41:47 +0100

    added countries list api request

--- src/components/AdditionalParams.tsx
+++ src/components/AdditionalParams.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import Dropdown from './Dropdown';
 import FilterCard from './FilterCard';
 import './AdditionalParams.css';
@@ -13,7 +14,7 @@ class AdditionalParams extends React.Component<Props> {
     state = {
         country: {
             checked: false,
-            value: 'spain'
+            value: 'ES'
         },
         explicit_content: {
             checked: false,
@@ -22,32 +23,39 @@ class AdditionalParams extends React.Component<Props> {
         limit: {
             checked: false,
             value: '10'
-        }
+        },
+        countriesData: []
     }
 
     onSearchUpdate = (name: string, value: string): void => {
         switch (name.toLowerCase()) {
             case 'country':
-                this.setState({country: {
-                    ...this.state.country,
-                    value: value.toLowerCase()}
+                this.setState({
+                    country: {
+                        ...this.state.country,
+                        value: value
+                    }
                 })
                 break;
 
             case 'explicit content':
-                this.setState({explicit_content: {
-                    ...this.state.explicit_content,    
-                    value: value.toLowerCase()}
+                this.setState({
+                    explicit_content: {
+                        ...this.state.explicit_content,
+                        value: value.toLowerCase()
+                    }
                 })
                 break;
-                
+
             case 'limit':
-                this.setState({limit: {
-                    ...this.state.limit,
-                    value: value}
+                this.setState({
+                    limit: {
+                        ...this.state.limit,
+                        value: value
+                    }
                 })
                 break;
-            
+
             default:
                 break;
         }
@@ -56,57 +64,71 @@ class AdditionalParams extends React.Component<Props> {
     onChecked = (name: string, value: string): void => {
         switch (name.toLowerCase()) {
             case 'country':
-                this.setState({country: {
-                    ...this.state.country,
-                    checked: value}
+                this.setState({
+                    country: {
+                        ...this.state.country,
+                        checked: value
+                    }
                 })
                 break;
 
             case 'explicit content':
-                this.setState({explicit_content: {
-                    ...this.state.explicit_content,
-                    checked: value}
+                this.setState({
+                    explicit_content: {
+                        ...this.state.explicit_content,
+                        checked: value
+                    }
                 })
                 break;
-                
+
             case 'limit':
-                this.setState({limit: {
-                    ...this.state.limit,
-                    checked: value}
+                this.setState({
+                    limit: {
+                        ...this.state.limit,
+                        checked: value
+                    }
                 })
                 break;
-            
+
             default:
                 break;
         }
     }
 
-    componentDidUpdate() {
-        this.props.callback(this.state)
+    componentDidMount() {
+        (async () => {
+            const result = await axios.get('https://www.liferay.com/api/jsonws/country/get-countries/');
+
+            this.setState({countriesData: result.data});
+        })();
     }
 
-    render(): JSX.Element {
-        return (
-            <div className="additionalParams">
-                <div className="additionalParams_header">
-                    <div className="additionalParams_header_line"></div>
-                    <p className="additionalParams_header_title">ADDITIONAL PARAMETERS</p>
-                    <div className="additionalParams_header_line"></div>
-                </div>
-                <div className="additionalParams_filters">
-                    <FilterCard onUpdate={this.onChecked} title="Country">
-                        <Dropdown onUpdate={this.onSearchUpdate} name="country" options={['Spain', 'France', 'Italy']} />
-                    </FilterCard>
-                    <FilterCard onUpdate={this.onChecked} title="Explicit Content">
-                        <Dropdown onUpdate={this.onSearchUpdate} name="explicit content" options={['Yes', 'No']} />
-                    </FilterCard>
-                    <FilterCard onUpdate={this.onChecked} title="Limit">
-                        <Input name="limit" onUpdate={this.onSearchUpdate} type="number" min={1} max={200} placeholder="1..200" />
-                    </FilterCard>
-                </div>
-            </div>
-        )
-    }    
-}
+    componentDidUpdate() {
+            this.props.callback(this.state)
+        }
 
-export default AdditionalParams
+        render(): JSX.Element {
+            return (
+                <div className="additionalParams">
+                    <div className="additionalParams_header">
+                        <div className="additionalParams_header_line"></div>
+                        <p className="additionalParams_header_title">ADDITIONAL PARAMETERS</p>
+                        <div className="additionalParams_header_line"></div>
+                    </div>
+                    <div className="additionalParams_filters">
+                        <FilterCard onUpdate={this.onChecked} title="Country">
+                            <Dropdown onUpdate={this.onSearchUpdate} name="country" options={this.state.countriesData} />
+                        </FilterCard>
+                        <FilterCard onUpdate={this.onChecked} title="Explicit Content">
+                            <Dropdown onUpdate={this.onSearchUpdate} name="explicit content" options={['Yes', 'No']} />
+                        </FilterCard>
+                        <FilterCard onUpdate={this.onChecked} title="Limit">
+                            <Input name="limit" onUpdate={this.onSearchUpdate} type="number" min={1} max={200} placeholder="1..200" />
+                        </FilterCard>
+                    </div>
+                </div>
+            )
+        }
+    }
+
+    export default AdditionalParams
--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -4,10 +4,27 @@ import './Dropdown.css';
 interface Props {
     onUpdate: any;
     name: string;
-    options: string[]
+    options: any
 }
 
 function Dropdown({ onUpdate, name, options }: Props): JSX.Element {
+    if (name === 'country') {
+
+        const countryList = options && options?.map((option: any): JSX.Element =>
+            <option key={option.a2} value={option.a2}>{option.name}</option>);
+
+        const onOptionChange = (e: any): void => {
+            onUpdate(e.target.name, e.target.value)
+        }
+
+        return (
+            <div className="dropdown">
+                <select name={name} onChange={(e) => { onOptionChange(e) }}>{countryList}</select>
+            </div>
+        );
+
+    }
+
     const optionsList = options.map((option: string, index: number): JSX.Element => {
         return <option key={index} value={option.toLowerCase()}>{option}</option>
     })
@@ -18,7 +35,7 @@ function Dropdown({ onUpdate, name, options }: Props): JSX.Element {
 
     return (
         <div className="dropdown">
-            <select name={name} onChange={(e) => {onOptionChange(e)}}>{optionsList}</select>
+            <select name={name} onChange={(e) => { onOptionChange(e) }}>{optionsList}</select>
         </div>
     )
 }
