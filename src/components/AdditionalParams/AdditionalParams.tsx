f04a823037d375207d5bb5f2ecaba2f667d308b7
victorgreco <victorgreco263@gmai.com>
2021-01-20 01:58:12 +0100

    create searchResultDisplay component

--- src/components/AdditionalParams/AdditionalParams.tsx
+++ src/components/AdditionalParams/AdditionalParams.tsx
@@ -5,79 +5,59 @@ import Input from '../Input/Input';
 import './AdditionalParams.css';
 
 interface FilterCardProps {
-    callback: Function;
+    setState: Function;
+    state: any;
 }
 
-class AdditionalParams extends React.Component<FilterCardProps> {
-    state = {
-        country: {
-            checked: false,
-            value: 'spain'
-        },
-        explicit_content: {
-            checked: false,
-            value: 'yes'
-        },
-        limit: {
-            checked: false,
-            value: '10'
-        }
-    };
-
-    updateState = (name: string, value: string | boolean): void => {
+function AdditionalParams({ setState, state }: FilterCardProps): JSX.Element {
+    const updateState = (name: string, value: string | boolean): void => {
         const lowerCaseName: string = name.toLowerCase();
         const lowerCaseValue: string | boolean = typeof value === 'string' && value.toLowerCase();
         const nameToState: any = {
             'country': {
                 country: {
-                    ...this.state.country,
+                    ...state.country,
                     value: lowerCaseValue
                 }
             },
             'explicit content': {
                 explicit_content: {
-                    ...this.state.explicit_content,
+                    ...state.explicit_content,
                     value: lowerCaseValue
                 }
             },
             'limit': {
                 limit: {
-                    ...this.state.limit,
+                    ...state.limit,
                     value: value
                 }
             },
-            default: this.state
+            default: state
         }
 
-        this.setState(nameToState[lowerCaseName]);
+        setState(nameToState[lowerCaseName]);
     };
 
-    componentDidUpdate() {
-        this.props.callback(this.state);
-    };
-
-    render(): JSX.Element {
-        return (
-            <div className="additionalParams">
-                <div className="additionalParams_header">
-                    <div className="additionalParams_header_line"></div>
-                    <p className="additionalParams_header_title">ADDITIONAL PARAMETERS</p>
-                    <div className="additionalParams_header_line"></div>
-                </div>
-                <div className="additionalParams_filters">
-                    <FilterCard onUpdate={this.updateState} title="Country">
-                        <Dropdown onUpdate={this.updateState} name="country" options={['Spain', 'France', 'Italy']} />
-                    </FilterCard>
-                    <FilterCard onUpdate={this.updateState} title="Explicit Content">
-                        <Dropdown onUpdate={this.updateState} name="explicit content" options={['Yes', 'No']} />
-                    </FilterCard>
-                    <FilterCard onUpdate={this.updateState} title="Limit">
-                        <Input name="limit" onUpdate={this.updateState} type="number" min={1} max={200} placeholder="1..200" />
-                    </FilterCard>
-                </div>
+    return (
+        <div className="additionalParams">
+            <div className="additionalParams_header">
+                <div className="additionalParams_header_line"></div>
+                <p className="additionalParams_header_title">ADDITIONAL PARAMETERS</p>
+                <div className="additionalParams_header_line"></div>
             </div>
-        );
-    };
-};
+            <div className="additionalParams_filters">
+                <FilterCard onUpdate={updateState} title="Country">
+                    <Dropdown onUpdate={updateState} name="country" options={['Spain', 'France', 'Italy']} />
+                </FilterCard>
+                <FilterCard onUpdate={updateState} title="Explicit Content">
+                    <Dropdown onUpdate={updateState} name="explicit content" options={['Yes', 'No']} />
+                </FilterCard>
+                <FilterCard onUpdate={updateState} title="Limit">
+                    <Input name="limit" onUpdate={updateState} type="number" min={1} max={200} placeholder="1..200" />
+                </FilterCard>
+            </div>
+        </div>
+    );
+}
 
-export default AdditionalParams
+export default AdditionalParams;
--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -4,41 +4,30 @@ import Input from '../Input/Input';
 import './Search.css';
 
 interface SearchProps {
-    callback: Function;
+    setState: Function;
 }
 
-class Search extends React.Component<SearchProps> {
-    state = {
-        category: 'song',
-        search_term: ''
-    }
-
-    onSearchUpdate = (name: string, value: string | boolean): void => {
+function Search({ setState }: SearchProps): JSX.Element {
+    const searchUpdate = (name: string, value: string | boolean): void => {
         const lowerCaseValue: string | boolean = typeof value === 'string' && value.toLowerCase();
         const getState = name === 'category' ?
             { category: lowerCaseValue } :
             { search_term: lowerCaseValue };
 
-        this.setState(getState);
-    }
+        setState(getState);
+    };
 
-    componentDidUpdate() {
-        this.props.callback(this.state);
-    }
-
-    render(): JSX.Element {
-        return (
-            <div className="search_container">
-                <div className="search_container_title">
-                    <h1>Music Library</h1>
-                </div>
-                <div className="search_container_fields">
-                    <Dropdown onUpdate={this.onSearchUpdate} name="category" options={['Song', 'Artist', 'Album', 'Music Video']} />
-                    <Input name="search_term" onUpdate={this.onSearchUpdate} type="text" placeholder="name..." />
-                </div>
+    return (
+        <div className="search_container">
+            <div className="search_container_title">
+                <h1>Music Library</h1>
             </div>
-        );
-    }
+            <div className="search_container_fields">
+                <Dropdown onUpdate={searchUpdate} name="category" options={['Song', 'Artist', 'Album', 'Music Video']} />
+                <Input name="search_term" onUpdate={searchUpdate} type="text" placeholder="name..." />
+            </div>
+        </div>
+    );
 }
 
 export default Search;
